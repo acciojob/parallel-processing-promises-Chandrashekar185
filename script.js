0a966643e0f8f79015fb0341a8f05866cfc8eb51
Chandrashekar185 <chanduminny32@gmail.com>
2025-02-24 23:12:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function downloadImages() {
+  const loadingDiv = document.getElementById("loading");
+  const errorDiv = document.getElementById("error");
+  const outputDiv = document.getElementById("output");
+
+  // Show loading spinner
+  loadingDiv.style.display = "block";
+  errorDiv.style.display = "none";
+  outputDiv.innerHTML = ""; 
+  Promise.all(imageUrls.map(downloadImage))
+    .then((images) => {
+
+      loadingDiv.style.display = "none";
+
+    
+      images.forEach((img) => outputDiv.appendChild(img));
+    })
+    .catch((error) => {
+    
+      loadingDiv.style.display = "none";
+      errorDiv.style.display = "block";
+      errorDiv.textContent = error.message;
+    });
+}
+
+downloadImages();
+
